bcede1e1e9fca0ce5ed1fb9b6f1b46cfaf058281
tamilee <tami.lee@gmail.com>
2017-12-11 20:45:24 -0800

    added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+##  Dummy file to compile and generate cython executables
+##
+## Sumeet Sandhu
+## Copyright (C) 2013- Elementary IP LLC
+#############################################
+
+
+from clusterPortfolio import generatePortfolio      # this comes from a compiled binary
+main ()
